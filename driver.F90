d148ac82fee3200c2fc432016c026bb1f868144f
syoukera <s.akira2986@gmail.com>
2019-08-15 18:18:15 +0900

    Add user defined explict time integration code

--- driver.F90
+++ driver.F90
@@ -38,7 +38,7 @@ program driver
 ! time integration
 !
   do while(time .lt. time_end)
-    call ZDPlasKin_timestep(time,dtime)
+    call ZDPlasKin_timestep_explicit_ck(time,dtime)
     time = time + dtime
     write(*,'(4(1pe12.4))') time, density(:)
   enddo
@@ -48,3 +48,70 @@ program driver
   write(*,'(/,A,$)') 'PRESS ENTER TO EXIT ...'
   read(*,*)
 end program driver
+
+!-----------------------------------------------------------------------------------------------------------------------------------
+!
+! timestep integration using explicit Euler method
+!
+!-----------------------------------------------------------------------------------------------------------------------------------
+subroutine ZDPlasKin_timestep_explicit_ck(time,dtime,rtol_loc,atol_loc,switch_implicit)
+  use ZDPlasKin
+  implicit none
+  double precision, intent(in) ::  time, rtol_loc, atol_loc
+  double precision, intent(inout) :: dtime
+  double precision, optional, intent(in) :: switch_implicit
+  double precision :: time_loc, time_end, dtime_loc, dtime_max
+  logical, save :: lwarn = .true.
+  if(.not. lZDPlasKin_init) call ZDPlasKin_init()
+  if( lqtplaskin .and. lqtplaskin_first ) call ZDPlasKin_write_qtplaskin(time)
+  if(rtol_loc <= 0.0d0) call ZDPlasKin_stop("ZDPlasKin ERROR: rtol_loc must be positive (subroutine ZDPlasKin_timestep_explicit)")
+  if(atol_loc <= 0.0d0) call ZDPlasKin_stop("ZDPlasKin ERROR: atol_loc must be positive (subroutine ZDPlasKin_timestep_explicit)")
+  tsav     = time
+  time_loc = 0.0d0
+  time_end = 0.5d0 * ( dtime + abs(dtime) ) + mach_tiny
+  do while(time_loc < time_end)
+    dens_loc(1:species_max,0) = density(:)
+    if( lgas_heating ) dens_loc(species_max+1,0) = ZDPlasKin_cfg(1)
+    dens_loc(:,1) = 0.5d0 * ( dens_loc(:,0) + abs( dens_loc(:,0) ) )
+    call ZDPlasKin_fex(vode_neq,tsav,dens_loc(:,1),dens_loc(:,2))
+    where(dens_loc(:,2) >= 0.0d0)
+      dens_loc(:,3) = + dens_loc(:,2) /    ( rtol_loc * dens_loc(:,1) + atol_loc ) 
+    elsewhere
+      dens_loc(:,3) = - dens_loc(:,2) / min( rtol_loc * dens_loc(:,1) + atol_loc , dens_loc(:,1) + mach_tiny )
+    endwhere
+    dtime_loc = 1.0d0 / ( maxval( dens_loc(:,3) ) + mach_tiny )
+    if(dtime > 0.0d0) then
+      dtime_max = dtime - time_loc
+      dtime_loc = min( dtime_loc , dtime_max )
+      if( present(switch_implicit) ) then
+        if(dtime_loc*switch_implicit < dtime_max) then
+          if(lprint .and. lwarn) then
+            write(*,"(A,/,A,1pd9.2,A)") "ZDPlasKin INFO: low efficiency of Euler method (subroutine ZDPlasKin_timestep_explicit)", &
+                        "                ZDPlasKin_timestep subroutine will be used in similar conditions (", switch_implicit, ")"
+            lwarn = .false.
+          endif
+          time_loc = tsav
+          density(:) = dens_loc(1:species_max,0)
+          if( lgas_heating ) ZDPlasKin_cfg(1) = dens_loc(species_max+1,0)
+          call ZDPlasKin_timestep(time_loc,dtime_max)
+          return
+        endif
+      endif
+    else
+      dtime = dtime_loc
+    endif
+    time_loc = time_loc + dtime_loc
+    tsav     = time     +  time_loc
+    density(:) = dens_loc(1:species_max,0) + dtime_loc * dens_loc(1:species_max,2)
+    if( lgas_heating ) ZDPlasKin_cfg(1) = dens_loc(species_max+1,0) + dtime_loc * dens_loc(species_max+1,2)
+  enddo
+  if( lstat_accum ) then
+    call ZDPlasKin_get_rates(SOURCE_TERMS=dens_loc(1:species_max,0),REACTION_RATES=rrt_loc)
+    stat_dens(:) = stat_dens(:) + dtime * density(:)
+    stat_src(:)  = stat_src(:)  + dtime * dens_loc(1:species_max,0)
+    stat_rrt(:)  = stat_rrt(:)  + dtime * rrt_loc(:)
+    stat_time    = stat_time    + dtime
+  endif
+  if( lqtplaskin ) call ZDPlasKin_write_qtplaskin(time+dtime)
+  return
+end subroutine ZDPlasKin_timestep_explicit_ck
